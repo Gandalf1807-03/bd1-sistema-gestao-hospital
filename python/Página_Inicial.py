d0bd1dc8ebaaea2252d1d06c4d44f1bc7e6f5aae
thgsergio <thiagolorax73@gmail.com>
2026-08-02 15:45:33 -0300

    etapa2/feat(front-end): Atualização da página inicial

--- python/Página_Inicial.py
+++ python/Página_Inicial.py
@@ -10,11 +10,17 @@ st.title("🏥 Sistema de Gestão Hospitalar")
 st.subheader("Hospital Universitário Dra. Yuska Maritan Brito")
 
 st.markdown("""
-Bem-vindo ao sistema de gestão! 
+Projeto desenvolvido para a cadeira de Banco de Dados I
+
+Equipe:
+- Gabriel Lorenzo Xavier
+- Jennifer Freire da Costa Silva
+- Luis Eduardo Pereira Nunes da Costa
+- Thiago Sergio Lima de Oliveira
 
 Utilize o **menu lateral esquerdo** para navegar entre os módulos do sistema:
-* **Cadastros:** Inserir pacientes, profissionais e atendimentos via ORM.
-* **Relatórios:** Visualizar painéis de internação e estatísticas (Views).
-* **Operações Avançadas:** Reajustar escalas e registrar atendimentos (Procedures).
-* **Validações:** Testar as regras de negócio e bloqueios do banco (Triggers).
+* **`Operações Avançadas`:** Reajustar escalas e registrar atendimentos (Procedures).
+* **`Validações`:** Testar as regras de negócio e bloqueios do banco (Triggers).
+* **`Relatórios`:** Visualizar painéis de internação e estatísticas (Views).
+* **`Cadastros`:** Inserir pacientes, profissionais e atendimentos via ORM.
 """)
